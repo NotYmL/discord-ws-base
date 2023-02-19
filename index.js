18294f9ec6407140906303b61904ddce890266e7
YmL <72396404+NotYmL@users.noreply.github.com>
2023-02-19 16:37:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const sleep = (milliseconds) => {return new Promise(resolve => setTimeout(resolv
 const fetch = require('node-fetch');
 const WebSocket = require('ws');
 
-let token = "OTQ1MDc0Mzk1NzA0MDc0Mjcx.G178to.90hUXve_KIzkVPj5RJqWIvkyRzSa3AxcSxr4gA";   // Dicord BOT token from https://discord.dev/
+let token = "xxx.xxx.xxx";   // Dicord BOT token from https://discord.dev/
 let intents = "32767"; // https://discord-intents-calculator.vercel.app/ and https://discord.com/developers/docs/topics/gateway#gateway-intents
 
 function recursion()
 {
-    let socket = new WebSocket("wss://gateway.discord.gg/?v=6&encording=json");
+    let socket = new WebSocket("wss://gateway.discord.gg/?encoding=json&v=9");
     socket.onready = function(event){
         console.log("WS Ready!")
     }
@@ -23,26 +23,45 @@ function recursion()
         if(ctx?.d?.heartbeat_interval!==undefined){
             var interval = JSON.parse(event.data)['d']['heartbeat_interval'];
             hb(socket, interval);
-            socket.send(JSON.stringify({ "op": 2, "d": { "intents": intents, "token": token, "properties": { "os": "BOT"}}}));
+            socket.send(JSON.stringify({
+                "op": 2, "d": { 
+                "intents": intents,
+                "token": token,
+                "properties":{
+                    "os":"Linux",
+                    "browser":"Firefox",
+                    "device":"",
+                },
+                "compress":false,
+                }
+            }));
         };
 
         switch (ctx["t"]) {
             case "READY":
-                READY_Event(ctx)
+                READY_Event(ctx);
+                break;
             case "CHANNEL_CREATE":
-                CHANNEL_CREATE_Event(ctx)
+                CHANNEL_CREATE_Event(ctx);
+                break;
             case "CHANNEL_DELETE":
-                CHANNEL_DELETE_Event(ctx) 
+                CHANNEL_DELETE_Event(ctx);
+                break;
             case "MESSAGE_CREATE":
-                MESSAGE_CREATE_Event(ctx)
+                MESSAGE_CREATE_Event(ctx);
+                break;
             case "MESSAGE_UPDATE":
                 MESSAGE_UPDATE_Event(ctx) 
+                break;
             case "MESSAGE_DELETE":
                 MESSAGE_DELETE_Event(ctx) 
+                break;
             case "MESSAGE_REACTION_ADD":
                 REACTION_ADD_Event(ctx)
+                break;
             case "MESSAGE_REACTION_REMOVE":
                 REACTION_REMOVE_Event(ctx) 
+                break;
             default:
                 All_other_events(ctx)
         }
